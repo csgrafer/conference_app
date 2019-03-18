ec8f2fc6f14e098254b8a033bafebe1e2177ec3f
“YOUR <2drum4fun@gmail.com>
2019-03-18 06:55:14 -0500

    populate db using ffaker info in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Speaker.create({first_name: "Dani", last_name: "Zaghian", email: "danielle@actualize.co"})
+
+FFaker::Name.first_name
+FFaker::Name.last_name
+FFaker::Internet.email
+
+9.times do
+  speaker1 = Speaker.new(first_name: FFaker::Name.first_name, last_name: FFaker::Name.last_name, email: FFaker::Internet.email)
+
+  speaker1.save
+end
